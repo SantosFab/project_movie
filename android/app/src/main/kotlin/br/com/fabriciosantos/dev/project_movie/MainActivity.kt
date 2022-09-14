256a44e13d526ab2e2593ae47b97e738ac44c2dd
SantosFab <fabriciosantos.dev@gmail.com>
2022-09-14 09:18:09 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/br/com/fabriciosantos/dev/project_movie/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.fabriciosantos.dev.project_movie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:project_movie/src/widget/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
