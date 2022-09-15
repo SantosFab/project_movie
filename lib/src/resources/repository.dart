91a9fd1de39c07d682604d2ad98a17193af0695d
SantosFab <fabriciosantos.dev@gmail.com>
2022-09-15 09:26:36 -0300

    modifico repository

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,7 +1,10 @@
 import 'package:project_movie/src/models/item_model.dart';
+import 'package:project_movie/src/models/trailer_model.dart';
 import 'package:project_movie/src/resources/movies_api.dart';
 
 class Repository {
   final moviesApi = MoviesApi();
+
   Future<ItemModel> fetchAllMovies() => moviesApi.fetchMovie();
+  Future<TrailerModel> fechallTrailer(id) => moviesApi.fetchTrailer(id);
 }
