ce0560d82a15065a822cbefbbbaa0c77f3ac8178
SantosFab <fabriciosantos.dev@gmail.com>
2022-09-15 09:26:10 -0300

    adiciono class responsavel por obter trailer

--- lib/src/resources/movies_api.dart
+++ lib/src/resources/movies_api.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'package:project_movie/src/models/item_model.dart';
+import 'package:project_movie/src/models/trailer_model.dart';
 
 class MoviesApi {
   final String _apiKey = 'a6d480541a151a410ba489d83fc81358';
@@ -12,6 +13,17 @@ class MoviesApi {
       return ItemModel.fromJson(response.body);
     } else {
       throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(String id) async {
+    var url = Uri.parse(
+        'https://api.themoviedb.org/3/movie/$id/videos?api_key=$_apiKey');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return TrailerModel.froJsom(response.body);
+    } else {
+      throw Exception('Failed to loead trailers');
     }
   }
 }
