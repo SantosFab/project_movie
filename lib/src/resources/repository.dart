9c5ba99f2f4de3a6a805dff10fefedf3af0bc67e
SantosFab <fabriciosantos.dev@gmail.com>
2022-09-19 16:23:06 -0300

    Modifico repository

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -5,6 +5,7 @@ import 'package:project_movie/src/resources/movies_api.dart';
 class Repository {
   final moviesApi = MoviesApi();
 
-  Future<ItemModel> fetchAllMovies(int page) => moviesApi.fetchMovie(page);
+  Future<ItemModel> fetchAllMovies({required int page, String? genre}) =>
+      moviesApi.fetchMovie(page: page, genre: genre);
   Future<TrailerModel> fechallTrailer(String id) => moviesApi.fetchTrailer(id);
 }
