0c3667879b80d95d288097cc4c9555022789e96a
SantosFab <fabriciosantos.dev@gmail.com>
2022-09-19 16:23:22 -0300

    adiciono TabBar

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,126 +1,80 @@
-import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
-import 'package:project_movie/src/blocs/floating_bloc.dart';
-import 'package:project_movie/src/blocs/movies_bloc.dart';
-import 'package:project_movie/src/models/item_model.dart';
-import 'package:project_movie/src/page/page_detail.dart';
-import 'package:project_movie/src/style/custom_color.dart';
+import 'package:project_movie/src/page/animacao_page.dart';
+import 'package:project_movie/src/page/drama_page.dart';
+import 'package:project_movie/src/page/ficcao_page.dart';
+import 'package:project_movie/src/page/popular_page.dart';
+import 'package:project_movie/src/widget/text_from.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late final MoviesBloc _blocMovies;
-  late final FloatingBloc _blocFloating;
-  int page = 1;
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  final GlobalKey<FormState> _fromKey = GlobalKey();
 
+  final TextEditingController _controller = TextEditingController();
+  late final TabController _tabController;
   @override
   void initState() {
+    _tabController = TabController(length: 4, vsync: this);
+
     super.initState();
-    _blocMovies = BlocProvider.getBloc<MoviesBloc>();
-    _blocFloating = BlocProvider.getBloc<FloatingBloc>();
-    _blocMovies.sink.add(page);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Filmes populares')),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: StreamBuilder<ItemModel>(
-          stream: _blocMovies.stream,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(
-                  'Ocorreu um error: ${snapshot.error}',
-                ),
-              );
-            }
-            if (snapshot.hasData) {
-              return _gridview(snapshot.data!);
-            }
-            return const Center(child: CircularProgressIndicator());
-          },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Filmes'),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(100),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  Form(
+                    key: _fromKey,
+                    child: TextFrom(
+                      controller: _controller,
+                      submitted: _getMovie,
+                    ),
+                  ),
+                  TabBar(
+                    labelPadding: const EdgeInsets.only(top: 14),
+                    controller: _tabController,
+                    tabs: const [
+                      Text('Popular'),
+                      Text('Ficção'),
+                      Text('Drama'),
+                      Text('Animação'),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: const <Widget>[
+            PopularPage(),
+            FiccaoPage(),
+            DramaPage(),
+            AnimacaoPage(),
+          ],
         ),
       ),
-      floatingActionButton: StreamBuilder<bool>(
-          stream: _blocFloating.stream,
-          initialData: true,
-          builder: (context, snapshot) {
-            if (page == 1) {
-              return FloatingActionButton(
-                backgroundColor: Colors.white,
-                onPressed: () {
-                  _blocMovies.sink.add(++page);
-                  if (page > 1) {
-                    _blocFloating.sink.add(true);
-                  }
-                },
-                child: const Icon(Icons.arrow_circle_right),
-              );
-            }
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FloatingActionButton(
-                  heroTag: 'btn1',
-                  onPressed: () {
-                    _blocMovies.sink.add(--page);
-                    if (page == 1) {
-                      _blocFloating.sink.add(false);
-                    }
-                  },
-                  backgroundColor: CustomColor.white,
-                  child: const Icon(Icons.arrow_circle_left),
-                ),
-                const SizedBox(width: 10),
-                FloatingActionButton(
-                  heroTag: 'btn2',
-                  backgroundColor: Colors.white,
-                  onPressed: () => _blocMovies.sink.add(++page),
-                  child: const Icon(Icons.arrow_circle_right),
-                ),
-              ],
-            );
-          }),
     );
   }
 
-  _gridview(ItemModel movies) {
-    return GridView.builder(
-      itemCount: movies.results.length,
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: () => Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => PageDetail(
-              id: movies.results[index].id,
-              title: movies.results[index].title,
-              overview: movies.results[index].overview,
-              releaseDate: movies.results[index].releaseDate,
-              backDropPath: movies.results[index].backdropPath,
-              voteAverage: movies.results[index].voteAverega,
-            ),
-          )),
-          child: Padding(
-            padding: const EdgeInsetsDirectional.all(4),
-            child: Image.network(
-              'https://image.tmdb.org/t/p/w500${movies.results[index].posterPath}',
-              fit: BoxFit.cover,
-            ),
-          ),
-        );
-      },
-    );
+  void _getMovie(String value) {
+    final FormState? from = _fromKey.currentState;
+    if (from != null && !from.validate()) {
+      return;
+    }
   }
 }
