b4f0a554f53f8e76852680f84501e5935d922933
SantosFab <fabriciosantos.dev@gmail.com>
2022-09-22 11:11:46 -0300

    corrijo altura

--- lib/src/widget/grid_display.dart
+++ lib/src/widget/grid_display.dart
@@ -9,34 +9,38 @@ class GridDisplay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: snapshot.data!.results.length,
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index) {
-        ItemModel movies = snapshot.data!;
-        return GestureDetector(
-          onTap: () => Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => PageDetail(
-                id: movies.results[index].id,
-                title: movies.results[index].title,
-                overview: movies.results[index].overview,
-                releaseDate: movies.results[index].releaseDate,
-                backDropPath: movies.results[index].backdropPath,
-                voteAverage: movies.results[index].voteAverega,
+    return SizedBox(
+      child: GridView.builder(
+        itemCount: snapshot.data!.results.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, mainAxisExtent: 250),
+        itemBuilder: (context, index) {
+          ItemModel movies = snapshot.data!;
+          return GestureDetector(
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => PageDetail(
+                  id: movies.results[index].id,
+                  title: movies.results[index].title,
+                  overview: movies.results[index].overview,
+                  releaseDate: movies.results[index].releaseDate,
+                  backDropPath: movies.results[index].backdropPath,
+                  voteAverage: movies.results[index].voteAverega,
+                ),
               ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsetsDirectional.all(4),
-            child: Image.network(
-              'https://image.tmdb.org/t/p/w500${movies.results[index].posterPath}',
-              fit: BoxFit.cover,
+            child: Padding(
+              padding: const EdgeInsetsDirectional.all(4),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.network(
+                  'https://image.tmdb.org/t/p/w500${movies.results[index].posterPath}',
+                ),
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
